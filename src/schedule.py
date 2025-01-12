8786792d43fba927ddc95a7551b85cb58002dbda
Slipchenko, Lyudmila V <lslipchenko@gmail.com>
2025-01-12 00:38:21 -0500

    schedule change

--- src/schedule.py
+++ src/schedule.py
@@ -126,18 +126,18 @@ class content:
 
 
 def build_schedule_s2022(srcdir):
-    schd = schedule('M', '2022-01-10')
+    schd = schedule('M', '2025-01-13')
 
-#     schd.add_holiday('2021-09-06', 'Labor Day')
+    #schd.add_holiday('2021-09-06', 'Labor Day')
     schd.add_holiday('2021-10-11', 'October Break (No labs)')
     schd.add_holiday('2021-10-12', 'October Break (No labs)')
     schd.add_holiday('2021-11-22', 'Thanksgiving (No labs)')
     
     #schd.add_holiday('2022-01-17', 'Martin Luther King Jr. Day')
-    schd.add_holiday('2022-03-14', 'Spring Vacation (No labs)')
-    schd.add_holiday('2022-03-15', 'Spring Vacation (No labs)')
-    schd.add_holiday('2022-03-16', 'Spring Vacation (No labs)')
-    schd.add_holiday('2022-03-17', 'Spring Vacation (No labs)')
+    schd.add_holiday('2025-03-18', 'Spring Vacation (No labs)')
+    schd.add_holiday('2025-03-19', 'Spring Vacation (No labs)')
+    schd.add_holiday('2025-03-20', 'Spring Vacation (No labs)')
+    schd.add_holiday('2025-03-21', 'Spring Vacation (No labs)')
 
     
 #     schd.add_holiday('2021-12-13', 'Finals')
